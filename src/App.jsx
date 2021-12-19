550b690107c0f0d5e22cc8ab1a15a69238857c99
koyasunatsunori <53142478+koyasunatsunori@users.noreply.github.com>
2021-12-19 21:20:57 +0900

    router-2120

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,9 @@
-import { useCallback, useMemo, useState } from "react";
-import { ChildArea } from "./components/ChildArea";
+import { Home } from "./Home";
 
 export default function App() {
-  console.log("app");
-  const [text, setText] = useState(0);
-  const [open, setOpen] = useState(false);
-
-  const onChangeText = (e) => setText(e.target.value);
-
-  const onClickOpen = () => setOpen(!open);
-
-  //useCallbackで囲む
-  //関数のメモ化
-  const onClickClose = useCallback(() => setOpen(false), [setOpen]);
-
-  //変数のメモ化、useMemo
-  //空配列を第二引数に設定→最初だけ読み込みが行われる
-  //利用頻度少なめ？
-  const temp = useMemo(() => 1 + 3, []);
-  console.log(temp);
-
   return (
     <div className="App">
-      <input value={text} onChange={onChangeText} />
-      <br />
-      <br />
-      <button onClick={onClickOpen}>表示</button>
-      <ChildArea open={open} onClickClose={onClickClose} />
+      <Home />
     </div>
   );
 }
---
+++ src/Page1.jsx
@@ -0,0 +1,7 @@
+export const Page1 = () => {
+  return (
+    <div>
+      <h1>Homeページです</h1>
+    </div>
+  );
+};
