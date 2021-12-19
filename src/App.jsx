571ccb2800091f3b25e1141cfdf8fb240f75257a
koyasunatsunori <53142478+koyasunatsunori@users.noreply.github.com>
2021-12-19 21:40:25 +0900

    react-route-2140

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
-import { Home } from "./Home";
+import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
 
+import { Home } from "./Home";
+import { Page1 } from "./Page1";
+import { Page2 } from "./Page2";
+
+//exact path完全一致のURLへルーティング
 export default function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Link to="/">Home</Link>
+        <br />
+        <Link to="/Page1">Page1</Link>
+        <br />
+        <Link to="/Page2">Page2</Link>
+      </div>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/page1">
+          <Page1 />
+        </Route>
+        <Route path="/page2">
+          <Page2 />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
--- src/Page1.jsx
+++ src/Page1.jsx
@@ -1,7 +1,7 @@
 export const Page1 = () => {
   return (
     <div>
-      <h1>Homeページです</h1>
+      <h1>Page1です</h1>
     </div>
   );
 };
