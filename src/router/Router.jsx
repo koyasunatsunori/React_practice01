55597fbec10bb6ae459c9d97b4eb2ced0b283b8d
koyasunatsunori <53142478+koyasunatsunori@users.noreply.github.com>
2021-12-22 09:11:23 +0900

    route-component-0911

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -4,6 +4,7 @@ import { Page1 } from "../Page1";
 import { Page1DetailA } from "../Page1DetailA";
 import { Page1DetailB } from "../Page1DetalB";
 import { Page2 } from "../Page2";
+import { Page1Routes } from "./Page1Routes";
 
 export const Router = () => {
   return (
@@ -15,15 +16,15 @@ export const Router = () => {
         path="/page1"
         render={({ match: { url } }) => (
           <Switch>
-            <Route exact path={url}>
-              <Page1 />
-            </Route>
-            <Route path={`${url}/detailA`}>
-              <Page1DetailA />
-            </Route>
-            <Route path={`${url}/detailB`}>
-              <Page1DetailB />
-            </Route>
+            {Page1Routes.map((route) => (
+              <Route
+                key={route.path}
+                exact={route.exact}
+                path={`${url}${route.path}`}
+              >
+                {route.children}
+              </Route>
+            ))}
           </Switch>
         )}
       />
