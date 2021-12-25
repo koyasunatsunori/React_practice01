a378c2aa54ba0d580af32dd6221d3444c4a7da60
koyasunatsunori <53142478+koyasunatsunori@users.noreply.github.com>
2021-12-25 10:19:31 +0900

    handle query parameter

--- src/Page2.jsx
+++ src/Page2.jsx
@@ -4,7 +4,10 @@ export const Page2 = () => {
   return (
     <div>
       <h1>Page2です</h1>
+
       <Link to="/page2/999">URL Parameter</Link>
+      <br />
+      <Link to="/page2/999?name=hogehoge">Query Parameter</Link>
     </div>
   );
 };
--- src/UrlParameter.jsx
+++ src/UrlParameter.jsx
@@ -1,11 +1,16 @@
-import { useParams } from "react-router-dom";
+import { useParams, useLoacation, useLocation } from "react-router-dom";
 
 export const UrlParameter = () => {
   const { id } = useParams();
+  const { search } = useLocation();
+  //クエリパラメータを扱うメソッドを提供
+  const query = new URLSearchParams(search);
+  console.log(query);
   return (
     <div>
       <h1>UrlParameterページです</h1>
       <p>パラメーターは{id}です</p>
+      <p>クエリパラメータは{query.get("name")}</p>
     </div>
   );
 };
